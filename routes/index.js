a522c2b3926e39759d787f2092543d321159695f
Wissem <wbousdira@gmail.com>
2023-10-19 11:09:09 +0200

    api

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 const fetch = require('node-fetch');
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 router.get('/movies', (req, res) => {
-	fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
+	fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
 		.then(response => response.json())
 		.then(data => {
 			res.json({ movies: data.results });
